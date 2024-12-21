316a5e3f21176020946a391fea7ee82155a796b4
Andrii Pesliak <apesliak@piraeusbank.ua>
2024-12-21 14:23:16 +0200

    Initial commit.

---
+++ src/main/java/io/spaceurgent/currency_rate_api/CurrencyRateApiApplication.java
@@ -0,0 +1,13 @@
+package io.spaceurgent.currency_rate_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyRateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyRateApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/spaceurgent/currency_rate_api/CurrencyRateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spaceurgent.currency_rate_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyRateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
