b794a6a43ef08f640c60bd4b6546b5e6ae240a5d
Andrii Pesliak <apesliak@piraeusbank.ua>
2024-12-23 01:17:05 +0200

    Create currency rate api client implementation.

---
+++ src/main/java/io/spaceurgent/currency/rate/api/client/CurrencyRateApiClientImpl.java
@@ -0,0 +1,44 @@
+package io.spaceurgent.currency.rate.api.client;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Flux;
+
+import static java.util.Objects.requireNonNull;
+
+@Slf4j
+public class CurrencyRateApiClientImpl implements CurrencyRateApiClient {
+    private final WebClient webClient;
+
+    public CurrencyRateApiClientImpl(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    @Override
+    public Flux<FiatCurrencyRateInfo> fetchFiatCurrencyRates() {
+        return doGet("/fiat-currency-rates", FiatCurrencyRateInfo.class);
+    }
+
+    @Override
+    public Flux<CryptoCurrencyRateInfo> fetchCryptoCurrencyRates() {
+        return doGet("/crypto-currency-rates", CryptoCurrencyRateInfo.class);
+    }
+
+    private <T> Flux<T> doGet(String uri, Class<T> returnClass) {
+        requireNonNull(uri, "Uri path is required");
+        requireNonNull(returnClass, "Return type is required");
+        return webClient.get().uri(uri)
+                .retrieve()
+                .bodyToFlux(returnClass)
+                .onErrorResume(WebClientResponseException.class, exception -> {
+                    log.error("Error calling GET '{}'. Response status '{}', response body: {}",
+                            uri, exception.getStatusCode(), exception.getResponseBodyAsString(), exception);
+                    return Flux.empty();
+                })
+                .onErrorResume(Throwable.class, throwable -> {
+                    log.error("Error calling GET '{}'. Error message: {}", uri, throwable.getMessage(), throwable);
+                    return Flux.empty();
+                });
+    }
+}
---
+++ src/test/java/io/spaceurgent/currency/rate/api/client/CurrencyRateApiClientImplTest.java
@@ -0,0 +1,198 @@
+package io.spaceurgent.currency.rate.api.client;
+
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.netty.http.client.HttpClient;
+import reactor.test.StepVerifier;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Predicate;
+
+import static io.spaceurgent.currency.rate.api.client.CurrencyRateApiConstants.API_KEY_HEADER_NAME;
+import static java.util.Objects.requireNonNull;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CurrencyRateApiClientImplTest {
+    private final static String MOCK_API_KEY = "test-key";
+    private final static String FIAT_CURRENCY_RATE_URI = "/fiat-currency-rates";
+    private final static String ERROR_RESPONSE_BODY = """
+                { "error" : "Internal server error" }
+            """;
+    private final static String FIAT_CURRENCY_RATES_RESPONSE_BODY = """
+            [
+                { "currency" : "USD", "rate" : 45.67 },
+                { "currency" : "EUR", "rate" : 56.78 }
+            ]
+            """;
+    private final static String CRYPTO_CURRENCY_RATES_RESPONSE_BODY = """
+            [
+                { "name" : "BTC", "value" : 12345.67 },
+                { "name" : "ETH", "value" : 234.56 }
+            ]
+            """;
+
+    private static MockWebServer mockWebServer;
+
+    private CurrencyRateApiClient currencyRateApiClient;
+
+    @BeforeAll
+    static void beforeAll() throws IOException {
+        mockWebServer = new MockWebServer();
+        mockWebServer.start();
+    }
+
+    @AfterAll
+    static void afterAll() throws IOException {
+        mockWebServer.shutdown();
+    }
+
+    @BeforeEach
+    void setUp() {
+        final var httpClient = HttpClient.create()
+                .responseTimeout(Duration.ofSeconds(2));
+        final var webClient = WebClient.builder()
+                .baseUrl("http://localhost:" + mockWebServer.getPort())
+                .defaultHeader(API_KEY_HEADER_NAME, MOCK_API_KEY)
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
+        currencyRateApiClient = new CurrencyRateApiClientImpl(webClient);
+    }
+
+    @Test
+    void fetchFiatCurrencyRates_shouldReturnCurrencyRateInfoFlux() throws InterruptedException {
+        mockWebServer.enqueue(
+                new MockResponse()
+                        .setBody(FIAT_CURRENCY_RATES_RESPONSE_BODY)
+                        .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+        );
+
+        final var rates = currencyRateApiClient.fetchFiatCurrencyRates();
+
+        StepVerifier.create(rates)
+                .expectNextMatches(fiatCurrencyRateEqualsPredicate("USD", BigDecimal.valueOf(45.67)))
+                .expectNextMatches(fiatCurrencyRateEqualsPredicate("EUR", BigDecimal.valueOf(56.78)))
+                .verifyComplete();
+        final var request = mockWebServer.takeRequest();
+        assertApiKeySent(request);
+        assertGetRequestPathIs(FIAT_CURRENCY_RATE_URI, request);
+    }
+
+    @Test
+    void fetchFiatCurrencyRates_shouldReturnEmptyFlux_whenApiReturnsError() throws InterruptedException {
+        mockWebServer.enqueue(
+                new MockResponse()
+                        .setResponseCode(500)
+                        .setBody(ERROR_RESPONSE_BODY)
+        );
+        final var rates = currencyRateApiClient.fetchFiatCurrencyRates();
+        assertEmptyFlux(rates);
+        final var request = mockWebServer.takeRequest();
+        assertApiKeySent(request);
+        assertGetRequestPathIs(FIAT_CURRENCY_RATE_URI, request);
+    }
+
+    @Test
+    void fetchFiatCurrencyRates_shouldReturnEmptyFlux_onRequestTimeout() throws InterruptedException {
+        mockWebServer.enqueue(
+                new MockResponse()
+                        .setBody(FIAT_CURRENCY_RATES_RESPONSE_BODY)
+                        .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .setBodyDelay(5, TimeUnit.SECONDS)
+        );
+
+        final var rates = currencyRateApiClient.fetchFiatCurrencyRates();
+
+        assertEmptyFlux(rates);
+        final var request = mockWebServer.takeRequest();
+        assertApiKeySent(request);
+        assertGetRequestPathIs(FIAT_CURRENCY_RATE_URI, request);
+    }
+
+    @Test
+    void fetchCryptoCurrencyRates_shouldReturnCurrencyRateInfoFlux() throws InterruptedException {
+        mockWebServer.enqueue(
+                new MockResponse()
+                        .setBody(CRYPTO_CURRENCY_RATES_RESPONSE_BODY)
+                        .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+        );
+
+        final var rates = currencyRateApiClient.fetchCryptoCurrencyRates();
+
+        StepVerifier.create(rates)
+                .expectNextMatches(cryptoCurrencyRateEqualsPredicate("BTC", BigDecimal.valueOf(12345.67)))
+                .expectNextMatches(cryptoCurrencyRateEqualsPredicate("ETH", BigDecimal.valueOf(234.56)))
+                .verifyComplete();
+
+        final var request = mockWebServer.takeRequest();
+        assertApiKeySent(request);
+        assertGetRequestPathIs("/crypto-currency-rates", request);
+    }
+
+    @Test
+    void fetchCryptoCurrencyRates_shouldReturnEmptyFlux_whenApiReturnsError() throws InterruptedException {
+        mockWebServer.enqueue(
+                new MockResponse()
+                        .setResponseCode(500)
+                        .setBody(ERROR_RESPONSE_BODY)
+        );
+        final var rates = currencyRateApiClient.fetchCryptoCurrencyRates();
+        assertEmptyFlux(rates);
+        final var request = mockWebServer.takeRequest();
+        assertApiKeySent(request);
+        assertGetRequestPathIs("/crypto-currency-rates", request);
+    }
+
+    @Test
+    void fetchCryptoCurrencyRates_shouldReturnEmptyFlux_onRequestTimeout() throws InterruptedException {
+        mockWebServer.enqueue(
+                new MockResponse()
+                        .setBody(CRYPTO_CURRENCY_RATES_RESPONSE_BODY)
+                        .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .setBodyDelay(5, TimeUnit.SECONDS)
+        );
+        final var rates = currencyRateApiClient.fetchCryptoCurrencyRates();
+        assertEmptyFlux(rates);
+        final var request = mockWebServer.takeRequest();
+        assertApiKeySent(request);
+        assertGetRequestPathIs("/crypto-currency-rates", request);
+    }
+
+    private static void assertApiKeySent(RecordedRequest recordedRequest) {
+        assertEquals(MOCK_API_KEY, recordedRequest.getHeader(API_KEY_HEADER_NAME));
+    }
+
+    private static void assertGetRequestPathIs(String uri, RecordedRequest request) {
+        assertEquals("GET", request.getMethod());
+        assertEquals(uri, request.getPath());
+    }
+
+    private static void assertEmptyFlux(Flux<?> flux) {
+        requireNonNull(flux, "Flux input is required");
+        StepVerifier.create(flux)
+                .expectNextCount(0)
+                .verifyComplete();
+    }
+
+    private static Predicate<FiatCurrencyRateInfo> fiatCurrencyRateEqualsPredicate(String currency, BigDecimal rate) {
+        return currencyRateInfo -> currency.equals(currencyRateInfo.getCurrency())
+                && rate.equals(currencyRateInfo.getRate());
+    }
+
+    private static Predicate<CryptoCurrencyRateInfo> cryptoCurrencyRateEqualsPredicate(String name, BigDecimal value) {
+        return currencyRateInfo -> name.equals(currencyRateInfo.getName())
+                && value.equals(currencyRateInfo.getValue());
+    }
+}
