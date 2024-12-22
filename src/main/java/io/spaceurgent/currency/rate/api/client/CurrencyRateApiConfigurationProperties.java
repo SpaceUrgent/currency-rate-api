b41472fdda41dfb1421c1d10839ee992fdb1c29d
Andrii Pesliak <apesliak@piraeusbank.ua>
2024-12-23 01:17:38 +0200

    Add currency rate api client bean configuration.

---
+++ src/main/java/io/spaceurgent/currency/rate/api/client/CurrencyRateApiConfigurationProperties.java
@@ -0,0 +1,24 @@
+package io.spaceurgent.currency.rate.api.client;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+//@Data
+@Validated
+@ConfigurationProperties(prefix = "currency-rate-api")
+public class CurrencyRateApiConfigurationProperties {
+    @NotNull
+    private String baseUrl;
+    @NotNull
+    private String secretKey;
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+}
---
+++ src/main/java/io/spaceurgent/currency/rate/api/client/CurrencyRateApiConstants.java
@@ -0,0 +1,8 @@
+package io.spaceurgent.currency.rate.api.client;
+
+public final class CurrencyRateApiConstants {
+    private CurrencyRateApiConstants() {
+    }
+
+    final static String API_KEY_HEADER_NAME = "X-API-KEY";
+}
