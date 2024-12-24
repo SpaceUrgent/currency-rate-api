6fb9c9027802579ce10427bb091fb07139a98d43
Andrii Pesliak <apesliak@piraeusbank.ua>
2024-12-24 14:14:23 +0200

    Update create table sql scripts.

--- src/main/resources/sql/create_crypto_rates_table.sql
+++ src/main/resources/sql/create_crypto_rates_table.sql
@@ -1,8 +1,8 @@
-CREATE TABLE public.crypto_rate (
+CREATE TABLE IF NOT EXISTS  public.crypto_rate (
     id BIGSERIAL PRIMARY KEY,
     inserted_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     name VARCHAR NOT NULL,
     value NUMERIC NOT NULL
 );
 
-CREATE INDEX crypto_rate_inserted_time_index ON public.crypto_rate (inserted_time);
+CREATE INDEX IF NOT EXISTS crypto_rate_inserted_time_index ON public.crypto_rate (inserted_time);
--- src/main/resources/sql/create_fiat_rates_table.sql
+++ src/main/resources/sql/create_fiat_rates_table.sql
@@ -1,8 +1,8 @@
-CREATE TABLE public.fiat_rate (
+CREATE TABLE IF NOT EXISTS  public.fiat_rate (
     id BIGSERIAL PRIMARY KEY,
     inserted_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     currency VARCHAR NOT NULL,
     rate NUMERIC NOT NULL
 );
 
-CREATE INDEX fiat_rate_inserted_time_index ON public.fiat_rate (inserted_time);
+CREATE INDEX IF NOT EXISTS fiat_rate_inserted_time_index ON public.fiat_rate (inserted_time);
